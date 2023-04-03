6b95e38b7642e8cfc6a6bf478521e66461b84122
Leandro <leandrobcrv@gmail.com>
2023-04-03 01:45:52 -0300

    Requisito 04

--- src/router/productsRouter.js
+++ src/router/productsRouter.js
@@ -1,10 +1,11 @@
 const { Router } = require('express');
 const productsController = require('../controllers/productsController');
+const productsValidator = require('../middlewares/productsValidator');
 
 const productsRouter = Router();
 
 productsRouter.get('/', productsController.getProducts);
 productsRouter.get('/:id', productsController.getProductsById);
-productsRouter.post('/', productsController.createNewProduct);
+productsRouter.post('/', productsValidator.validateProduct, productsController.createNewProduct);
 
 module.exports = productsRouter;
